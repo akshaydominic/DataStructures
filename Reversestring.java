a6324a48df610f7aa8aa7b67fe4d4b919611d5e4
Akshay A <adominic21@gmail.com>
2021-04-26 08:55:13 +0530

    added improved fun

--- Reversestring.java
+++ Reversestring.java
@@ -1,6 +1,13 @@
 //for a given string reverse the string input apple
 public class Reversestring{
     public static void main(String[] args){
-        System.out.println("Hello world");
+        String str="apple";
+        
+        String[] temp = str.split("");
+        String reverse="";
+        for(String c :temp){
+            reverse = c+reverse;
+        }
+        System.out.println("Result :"+reverse);
     }
 }
