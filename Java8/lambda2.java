d3c0c56fa6c5386a8b79627557ad9b7c1b2cbe20
Akshay A <adominic21@gmail.com>
2021-05-18 17:11:22 +0530

    added java 8

---
+++ Java8/lambda2.java
@@ -0,0 +1,7 @@
+class lambda2{
+    public static void main(String[] args) {
+       hellothere mylambda= ()->System.out.println("welcome to lambda expression");
+       mylambda.greeter();
+    }
+
+}
---
+++ Java8/lambdaeg.java
@@ -0,0 +1,11 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+class lambdaeg{
+    public static void main(String[] args) {
+        lambdaeg hi = new lambdaeg();
+        hi.gretter();        
+    }
+    public void gretter(){
+        System.out.println("Hello welcome");
+    }
+}
