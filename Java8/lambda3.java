d9030c804cc4176e481b1adaf5439969882d3279
Akshay A <adominic21@gmail.com>
2021-05-18 17:16:17 +0530

    added string length lambda

---
+++ Java8/lambda3.java
@@ -0,0 +1,6 @@
+class lambda3{
+    public static void main(String[] args) {
+        stringlengthinter mylambdaexpression=(s)->s.length();
+        System.out.println(mylambdaexpression.get("hello world"));
+    }
+}
---
+++ Java8/stringlengthinter.java
@@ -0,0 +1,3 @@
+public interface stringlengthinter{
+    public int get(String s);
+}
