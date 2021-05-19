714213d0f42348f347e3448022590a0fd9b8ce35
Akshay A <adominic21@gmail.com>
2021-05-19 10:14:33 +0530

    add

--- Java8/lambdaTest.java
+++ Java8/lambdaTest.java
@@ -1,17 +1,27 @@
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 class lambdaTest{
     public static void main(String[] args) {
         int[] numbers = {1,6,2,3,4,10,12,15};
-        findgreaterthan5(numbers,i->i>5);    
+        findgreaterthan5(numbers,wrapperLambda(i->System.out.println(i/2)));    
     }
 
-    private static void findgreaterthan5(int[] numbers,Predicate<Integer> p) {
+    private static void findgreaterthan5(int[] numbers,Consumer<Integer> p) {
         for(int i:numbers){
-            if(p.test(i)){
-                System.out.println(i);
-            }
+            p.accept(i);
         }
+    }
+    private static Consumer<Integer> wrapperLambda (Consumer<Integer> p){
+        return i->{
+            try {
+                p.accept(i);
+                
+            } catch (ArithmeticException e) {
+                System.out.println("Error found in Wrapper Class");
+            }
+
+        };
     }
 
 }
