00d5c5b45cb567941689dded8eb558518bf70f99
Akshay A <adominic21@gmail.com>
2021-05-19 17:12:01 +0530

    Streams Api

---
+++ Java8/StreamEg.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+
+import Excercise1.Person;
+
+public class StreamEg{
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+            new Person("Charles","Dickends",23),
+            new Person("akshay","A",23),
+            new Person("Lewis","Carrol",44),
+            new Person("Thomas","Carlyle",74),
+            new Person("Mathew","Arnold",53)
+        );
+
+        people.stream()
+        .filter(p->p.getLastName().startsWith("A"))
+        .forEach(p->System.out.println(p.getFirstName()));
+
+    }
+}
